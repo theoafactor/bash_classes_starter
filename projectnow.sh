d1a03b5d64c06e37b1d81a5284ce52c30c625690
Olu Adeyemo:Astronaut <theoafactor@gmail.com>
2024-04-25 21:39:03 +0100

    Solution to project

---
+++ projectnow.sh
@@ -0,0 +1,8 @@
+## Get two numbers from the terminal
+
+## 1. Calculate the difference and print the result
+## 2. What is the result of the first number modulus second number
+## 3. Write the script that checks if the first number is greater than or less than the second number
+## 4. Write a script that checks if a user's first name and last name are : James and John respectively
+
+
